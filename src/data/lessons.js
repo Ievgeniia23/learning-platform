1e7eb39c9d54ca1e159d311c4f4dd16472bc23c6
Ievgeniia23 <affari.nuovi.sempre@gmail.com>
2026-03-05 15:07:23 +0100

    Add LessonsList, LessonsPage, vercel.json

---
+++ src/data/lessons.js
@@ -0,0 +1,61 @@
+export const lessons = [
+  {
+    id: 'intro',
+    title: 'Introduction to JavaScript',
+    description: 'What JavaScript is and where it is used',
+    content: [
+      'JavaScript is a programming language used to make web pages interactive.',
+      'It runs in the browser and allows you to respond to user actions.',
+      'With JavaScript you can change content, handle events, and work with data.',
+    ],
+    task: 'Read the lesson and understand what JavaScript is used for.',
+  },
+
+  {
+    id: 'variables',
+    title: 'Variables',
+    description: 'Storing data in JavaScript',
+    content: [
+      'Variables are used to store data values.',
+      'You can create variables using let, const, or var.',
+      'Use const by default, and let when the value needs to change.',
+    ],
+    task: 'Create two variables: one with let and one with const.',
+  },
+
+  {
+    id: 'data-types',
+    title: 'Data Types',
+    description: 'Basic data types in JavaScript',
+    content: [
+      'JavaScript has several basic data types.',
+      'Some of them are string, number, boolean, null, and undefined.',
+      'Knowing data types helps you avoid bugs.',
+    ],
+    task: 'Write examples of string, number, and boolean values.',
+  },
+
+  {
+    id: 'functions',
+    title: 'Functions',
+    description: 'How to create and use functions',
+    content: [
+      'Functions are blocks of code that perform a specific task.',
+      'They help avoid repetition and make code easier to read.',
+      'Functions can accept parameters and return values.',
+    ],
+    task: 'Write a function that returns the sum of two numbers.',
+  },
+
+  {
+    id: 'arrays',
+    title: 'Arrays',
+    description: 'Working with lists of data',
+    content: [
+      'Arrays are used to store multiple values in one variable.',
+      'Each value in an array has an index starting from 0.',
+      'Arrays have useful methods like push, map, and filter.',
+    ],
+    task: 'Create an array with 3 values and add one more using push.',
+  },
+];
--- src/pages/LessonPage/LessonPage.jsx
+++ src/pages/LessonPage/LessonPage.jsx
@@ -1,9 +1,23 @@
-import css from "./LessonPage.module.css"
+import { useParams } from 'react-router-dom';
+
+import { lessons } from '../../data/lessons.js';
+
+import css from './LessonPage.module.css';
 
 const LessonPage = () => {
-  return (
-    <div>LessonPage</div>
-  )
-}
+  const { id } = useParams();
+  const lesson = lessons.find(l => l.id === id);
 
-export default LessonPage
+  if (!lesson) return <div>Lesson not found</div>;
+  return (
+    <div>
+      <h1>{lesson.title}</h1>
+      {lesson.content.map((p, index) => (
+        <p key={index}>{p}</p>
+      ))}
+      <p>Task: {lesson.task}</p>
+    </div>
+  );
+};
+
+export default LessonPage;
