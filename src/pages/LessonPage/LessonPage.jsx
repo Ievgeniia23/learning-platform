a5db2ee8d0f5db74a4e8fcfc39c754a5407b6233
Ievgeniia23 <affari.nuovi.sempre@gmail.com>
2026-03-06 11:39:29 +0100

    Add lesson s navigation and description

--- src/pages/LessonPage/LessonPage.jsx
+++ src/pages/LessonPage/LessonPage.jsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 import { lessons } from '../../data/lessons.js';
 
@@ -6,16 +6,31 @@ import css from './LessonPage.module.css';
 
 const LessonPage = () => {
   const { id } = useParams();
-  const lesson = lessons.find(l => l.id === id);
+
+  const lesson = lessons.find((lesson) => lesson.id === id);
+  const currentIndex = lessons.findIndex((lesson) => lesson.id === id);
+
+  const prevLesson = lessons[currentIndex - 1];
+  const nextLesson = lessons[currentIndex + 1];
 
   if (!lesson) return <div>Lesson not found</div>;
+
   return (
-    <div>
+    <div className={css.lessons}>
       <h1>{lesson.title}</h1>
+
       {lesson.content.map((p, index) => (
         <p key={index}>{p}</p>
       ))}
-      <p>Task: {lesson.task}</p>
+      <p>
+        <strong>Task:</strong> {lesson.task}
+      </p>
+
+      <div className={css.navigation}>
+        {prevLesson && <Link to={`/lesson/${prevLesson.id}`}>Previous</Link>}
+
+        {nextLesson && <Link to={`/lesson/${nextLesson.id}`}>Next</Link>}
+      </div>
     </div>
   );
 };
