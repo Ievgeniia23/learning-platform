219003b4591ced42d788f56e74b36ccc132f3c1f
Ievgeniia23 <affari.nuovi.sempre@gmail.com>
2026-03-06 14:31:26 +0100

    Add functions for navigation in list

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 
 import Layout from './components/Layout/Layout.jsx';
-import LessonsList from './pages/LessonsList/LessonsList.jsx';
+import Home from './pages/Home/Home.jsx';
 import LessonPage from './pages/LessonPage/LessonPage.jsx';
 
 import './App.css';
+import AllLessonsPage from './pages/AllLessonsPage/AllLessonsPage.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path="/" element={<LessonsList />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/lessons" element={<AllLessonsPage />} />
           <Route path="/lesson/:id" element={<LessonPage />} />
         </Routes>
       </Layout>
---
+++ src/pages/AllLessonsPage/AllLessonsPage.jsx
@@ -0,0 +1,10 @@
+import LessonsList from '../../components/LessonsList/LessonsList.jsx';
+
+import css from './AllLessonsPage.module.css';
+
+const AllLessonsPage = () => {
+  return;
+  <LessonsList />;
+};
+
+export default AllLessonsPage;
